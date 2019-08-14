d81233e66e30e2e751820c7b43d1b0bc366eea6f
Viswarajramji <viswarajramji93@gmail.com>
2019-08-14 19:44:03 +0530

    spring _MVC _Learning

---
+++ Spring_MVC/src/com/spring/mvc/MyController.java
@@ -0,0 +1,13 @@
+package com.spring.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyController {
+
+	@RequestMapping("/")
+	public String ShowPage() {
+		return "main-ui";
+	}
+}
