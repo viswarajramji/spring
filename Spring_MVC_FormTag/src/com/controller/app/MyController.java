77365d64d7e8d10e0b784a7dc52b9082ea0f6a0c
Viswarajramji <viswarajramji93@gmail.com>
2019-08-15 00:12:10 +0530

    spring MVC Form handling

---
+++ Spring_MVC_FormTag/src/com/controller/app/MyController.java
@@ -0,0 +1,33 @@
+package com.controller.app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.controller.pojo.Student;
+
+@Controller
+@RequestMapping("/hello")
+public class MyController {
+	@RequestMapping("/")
+	public String showHomePage() {
+		return "homepage";
+	}
+
+	@RequestMapping("/showForm")
+	public String showForm(Model model) {
+		model.addAttribute("student",new Student());
+		return "form-ui";
+	}
+	
+	
+	@RequestMapping("/processForm")
+	public String processForm(@ModelAttribute("student")Student student ,Model model) {
+		model.addAttribute("student",student);
+		System.out.println(student.getFirstName());
+		System.out.println(student.getLastName());
+		return "response-ui";
+	}
+
+}
---
+++ Spring_MVC_FormTag/src/com/controller/pojo/Student.java
@@ -0,0 +1,25 @@
+package com.controller.pojo;
+
+public class Student {
+	
+	String firstName;
+	
+	String lastName;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+}
