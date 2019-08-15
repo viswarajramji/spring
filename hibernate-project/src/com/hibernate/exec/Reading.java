86d0f80d0d7b89b0ef1ec3eef104c2d4a4d347d9
Viswarajramji <viswarajramji93@gmail.com>
2019-08-15 18:48:20 +0530

    Hibernate Query operation

--- hibernate-project/src/com/hibernate/exec/Reading.java
+++ hibernate-project/src/com/hibernate/exec/Reading.java
@@ -1,38 +1,32 @@
 package com.hibernate.exec;
 
-import java.util.List;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 import com.hibernate.entity.Student;
 
-public class Query {
+public class Reading {
 	public static void main(String[] args) {
 		SessionFactory sessionFactory = new Configuration().configure().addAnnotatedClass(Student.class)
 				.buildSessionFactory();
-		
 		try {
 			Session session = sessionFactory.getCurrentSession();
 			session.beginTransaction();
-			List<Student> students = session.createQuery("from Student").getResultList();
-			for (Student std : students) {
-				System.out.println(std.getEmailId() + " " + std.getFirstName() + " " + std.getLastName());
-			}
+			Student s1 = new Student("Guru", "advanth", "Guru93@gmail.com");
+			session.save(s1);
 			session.getTransaction().commit();
 			session.close();
+			System.out.println(s1.getId());
+			// ------------------//
+			// query new object
 			session = sessionFactory.getCurrentSession();
-			//where conditions
-			System.out.println("--------------select by Guru--------------");
 			session.beginTransaction();
-			students = session.createQuery("from Student s where s.firstName = 'Guru'").getResultList();
-			for (Student std : students) {
-				System.out.println(std.getEmailId() + " " + std.getFirstName() + " " + std.getLastName());
-			}
+			Student s2 = session.get(Student.class, s1.getId());
 			session.getTransaction().commit();
+			System.out.println(s2.toString());
+
 			session.close();
-			
 		} finally {
 			sessionFactory.close();
 		}
