738c5183743c6f27408bdb23e42164d2f16ee63a
Viswarajramji <viswarajramji93@gmail.com>
2019-08-17 14:35:10 +0530

    Handle CSRF & taglibs username and role

---
+++ spring-csrf-token/src/main/java/com/spring/config/WebSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.spring.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		UserBuilder users = User.withDefaultPasswordEncoder();
+		auth.inMemoryAuthentication().withUser(users.username("shan").password("shan").roles("EMPLOYEE"))
+				.withUser(users.username("guru").password("guru").roles("MANAGER"));
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// TODO Auto-generated method stub
+		http.authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/showLogin")
+				.loginProcessingUrl("/authenticateTheUser").permitAll().and().logout().permitAll();
+	}
+}
