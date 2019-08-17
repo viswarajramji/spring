2b27870fd06411f8b7551f2d0be86ab95f454417
Viswarajramji <viswarajramji93@gmail.com>
2019-08-18 00:39:55 +0530

    @Path Variables

--- test/src/main/java/com/spring/controllers/MyController.java
+++ test/src/main/java/com/spring/controllers/MyController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,19 +14,27 @@ import com.pojo.Student;
 @RequestMapping("/")
 public class MyController {
 
+	List<Student> list=new ArrayList<>();
+
+	
+	
 	@GetMapping("/test")
 	public String gethome() {
+		list.add(new Student(1, "viswa1", "viswaraj93@gmail.com"));
+		list.add(new Student(2, "viswa2", "viswaraj93@gmail.com"));
+		list.add(new Student(3, "viswa3", "viswaraj93@gmail.com"));
+		list.add(new Student(4, "viswa4", "viswaraj93@gmail.com"));
 		return "hello dammer";
 	}
 	
 	
 	@GetMapping("/listdata")
 	public List<Student> getListData(){
-		List<Student> list=new ArrayList<>();
-		list.add(new Student(1, "viswa1", "viswaraj93@gmail.com"));
-		list.add(new Student(2, "viswa2", "viswaraj93@gmail.com"));
-		list.add(new Student(3, "viswa3", "viswaraj93@gmail.com"));
-		list.add(new Student(4, "viswa4", "viswaraj93@gmail.com"));
 		return list;
+	}
+	
+	@GetMapping("/student/{student_id}")
+	public Student getStudent(@PathVariable int student_id) {
+		return list.get(student_id);
 	}
 }
